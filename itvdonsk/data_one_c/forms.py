797dfb51f92ecc524cf458bac32427811603f5ae
romancvc <roman.chechevichko@gmail.com>
2023-03-15 15:24:58 +0300

    Добавление команды бота

---
+++ itvdonsk/data_one_c/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+from data_one_c.models import Client
+
+
+class ClientForm(forms.ModelForm):
+
+    class Meta:
+        model = Client
+        fields = (
+            'full_name',
+            'INN',
+            'status',
+        )
+        widgets = {
+            'full_name': forms.TextInput,
+        }
